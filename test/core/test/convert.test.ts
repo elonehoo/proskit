9339e5a81023169ec51d9f4cfc58d071f1027209
xiaoxunyao <huchengyea@163.com>
2022-04-02 14:12:54 +0800

    test: update

--- test/core/test/convert.test.ts
+++ test/core/test/convert.test.ts
@@ -1,6 +1,15 @@
 
 import { assert, expect, test } from 'vitest'
 
-test('text start',()=>{
+import { strUtil } from '../../../packages/toolbox/src/main'
+
+test.skip('text start',()=>{
   console.log('text start')
+})
+
+test('test in strUtil', () => {
+  console.log('strUtil is blank -->',strUtil.isBlank(''))
+  console.log('strUtil is blank -->',strUtil.isBlank('abc'))
+  console.log('strUtil is blank -->',strUtil.isNotEmpty(' abc '))
+  console.log('strUtil is blank -->',strUtil.isNotEmpty(''))
 })
