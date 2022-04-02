9a3a79b9fa1839dbf299c2bc29284cd6e817f904
xiaoxunyao <huchengyea@163.com>
2022-04-02 15:58:45 +0800

    chore: init

---
+++ test/util_test/test/convert.test.ts
@@ -0,0 +1,15 @@
+
+import { assert, expect, test } from 'vitest'
+
+import { strUtil } from '../../../packages/util/src/main'
+
+test.skip('text start',()=>{
+  console.log('text start')
+})
+
+test('test in strUtil', () => {
+  console.log('strUtil is blank -->',strUtil.isBlank(''))
+  console.log('strUtil is blank -->',strUtil.isBlank('abc'))
+  console.log('strUtil is blank -->',strUtil.isNotEmpty(' abc '))
+  console.log('strUtil is blank -->',strUtil.isNotEmpty(''))
+})
